528d81ce5fcf8b959516229133876ad62ee9a9ba
SaraDoesIt <sclark@foghornconsulting.com>
2025-07-23 19:26:31 -0400

    Remove bson.dumps() in place of a JSON encoder to production readiness.

--- routes.py
+++ routes.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, request, jsonify
+import json
+from flask import Blueprint, request, jsonify, Response
 from datetime import datetime
-from bson.json_util import dumps
+from bson import ObjectId
 from query_utils import build_query, build_stats_pipeline
 from models import validate_event
 from db import mongo
+
+# JSON Encoder
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super().default(obj)
 
 events_bp = Blueprint('events', __name__)
 
@@ -22,12 +32,12 @@ def add_event():
 @events_bp.route('/events', methods=['GET'])
 def get_events():
     query = build_query(request.args)
-    events = mongo.db.events.find(query)
-    return dumps(events), 200
+    events = list(mongo.db.events.find(query))
+    return Response(json.dumps(events, cls=JSONEncoder), mimetype='application/json'), 200
 
 # GET /events/stats
 @events_bp.route('/events/stats', methods=['GET'])
 def get_event_stats():
     pipeline = build_stats_pipeline()
     stats = list(mongo.db.events.aggregate(pipeline))
-    return dumps(stats), 200
+    return Response(json.dumps(stats, cls=JSONEncoder), mimetype='application/json'), 200
